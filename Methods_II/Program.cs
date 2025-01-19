6242ee29ec51bcc46922670fee8c8428545238db
Gustavo Perez <gustavo.perez.231191@gmail.com>
2025-01-19 16:47:19 -0600

    add methods part 2

---
+++ Methods_II/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Methods_II
+{
+    internal class Program
+    {
+        //Fields (or instance variable) - sometimes called global variable
+        //int num;
+        //float num2;
+
+        static void Main(string[] args)
+        {
+            Program myProgram = new Program();
+            int num = myProgram.ReturnANumber(1);
+
+            Console.WriteLine(num);
+
+            float num2 = ReturnAFloat(3.3f);
+        }
+
+        int ReturnANumber(int num)
+        {
+            return num;
+        }
+
+        static float ReturnAFloat(float num)
+        {
+            return num;
+        }
+    }
+}
