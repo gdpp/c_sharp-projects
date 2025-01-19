f981a7a7f89b92aa9c5a8e34bc169cc8009081d4
Gustavo Perez <gustavo.perez.231191@gmail.com>
2025-01-19 15:09:51 -0600

    add loops project

---
+++ Loops/Program.cs
@@ -0,0 +1,72 @@
+﻿// FOR LOOP
+
+for (int i = 0; i < 3; i++)
+{
+    Console.WriteLine("I is: " + i);
+}
+
+string myStr = "Hi \nHi";
+
+for (int counter = 0; counter <= 5 ; counter++)
+{
+    Console.WriteLine("Counter is: " + counter);
+    Console.WriteLine(myStr);
+    Thread.Sleep(1000);
+}
+
+// WHILE LOOP
+
+bool condition = true;
+
+while (condition)
+{
+    Console.WriteLine("While running ");
+
+    for (int i = 0; i < 3; i++)
+    {
+        Console.WriteLine(i);
+        condition = false;
+    }
+}
+
+Console.WriteLine("End while");
+
+
+// DO WHILE
+
+bool isAlive = true;
+do
+{
+
+    Console.WriteLine("Continue? (yes/no)");
+    string res = Console.ReadLine();
+
+    if (res.Trim().ToLower() != "yes") { 
+        isAlive = false;
+    }
+}
+while (isAlive);
+
+
+
+// ARRAYS
+
+int[] numbers = new int[3];
+
+numbers[0] = 1;
+numbers[1] = 2;
+numbers[2] = 3;
+
+bool[] states = [true, false];
+
+// FOREACH LOOP
+
+foreach (var item in numbers)
+{
+    Console.WriteLine($"Item: {item}");
+}
+
+Console.ReadKey();
+
+
+
