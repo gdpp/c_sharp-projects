665fbe3ecac02a04a88099497432bc3ca069f65f
Gustavo Perez <gustavo.perez.231191@gmail.com>
2025-01-19 15:31:44 -0600

    add methods part 1

---
+++ Methods/Program.cs
@@ -0,0 +1,22 @@
+﻿void FirstMethod()
+{
+    Console.WriteLine("Hi!");
+    Console.WriteLine("First Method");
+}
+
+void Greet(string name)
+{
+    Console.WriteLine($"Hello {name}, nice to meet you.");
+}
+
+int Add(int number1, int number2)
+{
+    return number1 + number2;
+}
+
+FirstMethod();
+Greet("Gustavoid");
+int result = Add(3, 4);
+
+Console.WriteLine(result);
+
