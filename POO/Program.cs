37421a027ce66c2de0f3e1abea501637e26921b3
Gustavo Perez <gustavo.perez.231191@gmail.com>
2025-01-21 16:29:38 -0600

    add POO_I
    short program to practice POO

---
+++ POO/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Net.NetworkInformation;
+using System.Numerics;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace POO
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Question[] questions = new Question[]
+            {
+                new Question(
+                    "In the \"Star Wars\" universe, what is the name of Han Solo's ship?",
+                    new string[] {
+                        "The Millennium Falcon", 
+                        "The Star Destroyer", 
+                        "The X-Wing", 
+                        "The TIE Fighter"
+                    }, 
+                    0
+                ),
+                new Question(
+                    "What is the primary programming language used to build Android apps?",
+                    new string[] {
+                        "Python",
+                        "Swift",
+                        "Java",
+                        "Ruby"
+                    },
+                    2
+                ),
+                new Question(
+                    "In the Marvel Cinematic Universe, who wields the Infinity Gauntlet to undo Thanos' snap?",
+                    new string[] {
+                        "Thor",
+                        "Iron Man",
+                        "Captain America",
+                        "Doctor Strange"
+                    },
+                    1
+                ),
+                new Question(
+                    "Which video game series features the character \"Master Chief\"?",
+                    new string[] {
+                        "Call of Duty",
+                        "Halo",
+                        "Doom",
+                        "Gears of War"
+                    },
+                    1
+                ),
+                new Question(
+                    "In the Harry Potter series, what is the name of Harry’s pet owl?",
+                    new string[] {
+                        "Errol",
+                        "Crookshanks",
+                        "Scabbers",
+                        "Hedwig"
+                    },
+                    3
+                ) 
+            };
+
+            Quiz quiz = new Quiz(questions);
+
+            quiz.StartQuiz();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ POO/Quiz.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace POO
+{
+    internal class Quiz
+    {
+        private Question[] _questions;
+        private int _score;
+
+        public Quiz(Question[] questions)
+        {
+            this._questions = questions;
+            _score = 0;
+        }
+
+        public void StartQuiz()
+        {
+            int questionNumber = 1;
+
+            foreach (Question question in _questions)
+            {
+                Console.WriteLine($"Question {questionNumber++}");
+                DisplayQuestion(question);
+                int usrChoice = GetUserChoice();
+
+                if (question.IsCorrectAnswer(usrChoice))
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("Correct!");
+                    Console.ResetColor();
+
+                    _score++;
+                }
+                else
+                {
+                    Console.ForegroundColor= ConsoleColor.Red;
+                    Console.WriteLine($"Wrong! ");
+                    Console.ResetColor();
+
+                    Console.ForegroundColor = ConsoleColor.Magenta;
+                    Console.WriteLine($"The correct answer was: {question.Answers[question.CorrectAnswerIndex]}");
+                    Console.ResetColor();
+                }
+            }
+
+            DisplayResult();
+        }
+
+        private void DisplayResult()
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("╔═════════════════════════════════════════════════════════════════════════╗");
+            Console.WriteLine("║                                 Results                                 ║");
+            Console.WriteLine("╚═════════════════════════════════════════════════════════════════════════╝");
+            Console.ResetColor();
+
+            Console.WriteLine($"Quiz finished. Your score is: {_score} out of {_questions.Length}");
+
+            double percentage = (double) _score / _questions.Length;
+
+            if (percentage > 0.9)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Good Effort!");
+                Console.ResetColor();
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Keep practicing!");
+                Console.ResetColor();
+            }
+        }
+
+        private void DisplayQuestion(Question question)
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("╔═════════════════════════════════════════════════════════════════════════╗");
+            Console.WriteLine("║                                 Question                                ║");
+            Console.WriteLine("╚═════════════════════════════════════════════════════════════════════════╝");
+            Console.ResetColor();
+
+            Console.WriteLine(question.QuestionText);
+
+            for (int i = 0; i < question.Answers.Length; i++)
+            {
+                Console.ForegroundColor = ConsoleColor.Cyan; // Changes text color
+                Console.Write("   ");
+                Console.Write($"{i + 1}");
+                Console.ResetColor(); // Resets color
+
+                Console.WriteLine($". {question.Answers[i]}");
+            }
+        }
+
+        private int GetUserChoice()
+        {
+            Console.Write("Your answer (number): ");
+
+            string input = Console.ReadLine();
+            int choice = 0;
+
+            while (!int.TryParse(input, out choice) || choice < 1 || choice > 4)
+            {
+                Console.Write("Invaid choice. Please enter a number between 1 and 4: ");
+                input = Console.ReadLine();
+            }
+
+            return choice - 1;
+        }
+    }
+}
