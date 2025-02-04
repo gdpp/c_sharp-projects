f28da5dc8249e613cb1a4da06d1e196fea957704
Gustavo Perez <gustavo.perez.231191@gmail.com>
2025-02-04 16:33:40 +0100

    Add errorHandling project

---
+++ ErrorHandling/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Diagnostics;
+
+namespace ErrorHandling
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int result = 0;
+
+            Debug.WriteLine("Main Method is running");
+
+            try
+            {
+                Console.Write("Please enter a number: ");
+                int num1 = int.Parse(Console.ReadLine());
+
+                int num2 = 2;
+
+                result = num1 / num2;
+
+                Console.WriteLine($"Result: {result}");
+            }
+            catch(Exception e) {
+                Console.WriteLine(e.ToString());
+            }
+            finally
+            {
+                // Code to cleanup or finalize,
+                // ideal for cleaning up resources,
+                // like closing file streams or database connection.
+                Console.WriteLine("This always executes");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
