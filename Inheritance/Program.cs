fc4ced12aca5e4e2f502a2111a096efdae82618c
Gustavo Perez <gustavo.perez.231191@gmail.com>
2025-02-05 11:39:36 +0100

    add inheritance

---
+++ Inheritance/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Inheritance
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+
+    class Animal
+    {
+        public void Eat()
+        {
+            Console.WriteLine("Eating...");
+        }
+    }
+
+    class Dog: Animal
+    {
+        public void Bark()
+        {
+            Console.WriteLine("Barking...");
+        }
+    }
+    
+    class Cat: Animal
+    {
+        public void Miau()
+        {
+            Console.WriteLine("Miauing...");
+        }
+    }
+}
+
